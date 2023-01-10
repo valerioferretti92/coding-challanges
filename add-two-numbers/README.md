d42daedda469cba3826687096682a188062fff46
Valerio Ferretti <valerio.ferretti92@gmail.com>
2023-01-10 22:45:34 +0100

    Leetcode, sum of two integers

---
+++ add-two-numbers/README.md
@@ -0,0 +1,32 @@
+# Add two numbers
+
+### Link
+https://leetcode.com/problems/add-two-numbers/
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+### Example 1:
+```
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+```
+
+### Example 2:
+```
+Input: nums = [3,2,4], target = 6V
+Output: [1,2]
+```
+
+### Example 3:
+```
+Input: nums = [3,3], target = 6
+Output: [0,1]
+```
+
+### Constrains
+- The number of nodes in each linked list is in the range [1, 100].
+- 0 <= Node.val <= 9
+- It is guaranteed that the list represents a number that does not have leading zeros.
---
+++ add-two-numbers/solution.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1.val == 0 && l1.next == null) {
+            return l2;
+        }
+        if (l2.val == 0 && l2.next == null) {
+            return l1;
+        }
+
+        ListNode tmp1 = l1, tmp2 = l2, result = new ListNode();
+        ListNode current = result;
+        int carry = 0;
+        while (true) {
+            if (tmp1 != null && tmp2 != null) {
+                current.val = (tmp1.val + tmp2.val + carry) % 10;
+                carry = (tmp1.val + tmp2.val + carry - current.val) / 10;
+            } else if (tmp1 != null && tmp2 == null) {
+                current.val = (tmp1.val + carry) % 10;
+                carry = (tmp1.val + carry - current.val) / 10;
+            } else if (tmp1 == null && tmp2 != null) {
+                current.val = (tmp2.val + carry) % 10;
+                carry = (tmp2.val + carry - current.val) / 10;
+            } else if (tmp1 == null && tmp2 == null && carry != 0) {
+                current.val = carry % 10;
+                carry = (carry - current.val) / 10;
+            }
+
+            if (tmp1 != null) {
+                tmp1 = tmp1.next;
+            }
+            if (tmp2 != null) {
+                tmp2 = tmp2.next;
+            }
+            if (tmp1 != null || tmp2 != null || carry != 0) {
+                current.next = new ListNode();
+                current = current.next;
+                continue;
+            }
+            return result;
+        }
+    }
+}
