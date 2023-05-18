2203282c6444496cefcfb462b3dfeb1d89a984b0
Valerio Ferretti <valerio.ferretti92@gmail.com>
2023-05-18 18:45:53 +0200

    LeetCode, correction of longer palindromic substring solutions

--- longest-palindromic-substring/main.go
+++ longest-palindromic-substring/main.go
@@ -3,32 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(longestPalindrome2("azsesq"))
+	fmt.Println(longestPalindrome("azsesq"))
 }
 
-/***************** Recursive solution *****************/
-
-func longestPalindromes1(s string) string {
-	// Base case 1
-	if isPalindrome(string(s)) {
-		return s
-	}
-
-	// Divide
-	lpal := longestPalindrome2(s[1:])
-	rpal := longestPalindrome2(s[0 : len(s)-1])
-
-	// Conquer
-	if len(lpal) > len(rpal) {
-		return lpal
-	} else {
-		return rpal
-	}
-}
-
-/***************** Linear solution *****************/
-
-func longestPalindrome2(s string) string {
+func longestPalindrome(s string) string {
 	if len(s) == 0 {
 		return ""
 	}
@@ -61,47 +39,4 @@ func max(a int, b int) int {
 		return a
 	}
 	return b
-}
-
-/***************** Quadratic solution *****************/
-
-func longestPalindrome3(s string) string {
-	if len(s) <= 1 {
-		return s
-	}
-
-	for i := len(s); i > 0; i-- {
-		palindrome, ok := lookUpPalindromes(i, s)
-		if ok {
-			return palindrome
-		}
-	}
-
-	return ""
-}
-
-func lookUpPalindromes(l int, s string) (string, bool) {
-	var start, end int = 0, l
-
-	for end <= len(s) {
-		if isPalindrome(s[start:end]) {
-			return s[start:end], true
-		}
-
-		start++
-		end++
-	}
-
-	return "", false
-}
-
-/***************** Utils *****************/
-
-func isPalindrome(s string) bool {
-	for i := 0; i < len(s); i++ {
-		if s[i] != s[len(s)-i-1] {
-			return false
-		}
-	}
-	return true
 }
